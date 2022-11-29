2dc18760d9b961344cf7af12db1721615f21704f
“Rebecca <rebeccaschiffman@gmail.com>
2022-11-28 17:15:45 -0800

    Write tests for ship

--- code/script.js
+++ code/script.js
@@ -1,8 +1,11 @@
 const shipFactory = (name, length) => {
   let hits = 0;
-  const hit = () => hits++;
-  const isSunk = () => hits >= length ? True : False;
-  return {name, length, hits, hit, isSunk};
+  const hit = () => hits = hits+1;
+  const isSunk = () => hits >= length ? true : false;
+  const getHits = () => hits;
+  return {name, length, hit, isSunk, getHits};
 };
+
+export { shipFactory };
 
 
---
+++ tests/script.test.js
@@ -0,0 +1,21 @@
+import { shipFactory } from "../code/script";
+
+test('is new ship sunk?', () => {
+  const battleship = shipFactory(battleship, 4);
+  expect(battleship.isSunk()).toBe(false);
+  });
+
+test('is new ship sunk?', () => {
+  const battleship = shipFactory(battleship, 4);
+  battleship.hit();
+  expect(battleship.getHits()).toBe(1);
+});
+
+test('does ship sink?', () => {
+  const battleship = shipFactory(battleship, 4);
+  battleship.hit();
+  battleship.hit();
+  battleship.hit();
+  battleship.hit();
+  expect(battleship.isSunk()).toBe(true);
+});
