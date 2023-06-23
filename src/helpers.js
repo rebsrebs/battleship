e06e5f6babf7d2f84c887d83304f322b25353ebc
“Rebecca <rebeccaschiffman@gmail.com>
2023-06-22 22:54:16 -0700

    Hardcode waitTime and sunkLimit for now

--- src/helpers.js
+++ src/helpers.js
@@ -2,15 +2,18 @@ const id = (ID) => document.getElementById(ID);
 
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
-let isTesting = false;
-let waitTime = Number(0);
-let sunkLimit = Number(1);
-if (isTesting == false) {
-  waitTime = Number(600);
-  sunkLimit = Number(5);
-} else {
-  waitTime = Number(0);
-  sunkLimit = Number(1);
-}
+// let isTesting = false;
+// let waitTime = Number(0);
+// let sunkLimit = Number(1);
+// if (isTesting == false) {
+//   waitTime = Number(600);
+//   sunkLimit = Number(5);
+// } else {
+//   waitTime = Number(0);
+//   sunkLimit = Number(1);
+// }
+
+let waitTime = Number(600);
+let sunkLimit = Number(5);
 
 export { id, delay, waitTime, sunkLimit };
